18ff3b5a54b0d075a9a20e7de7f3f750eab36fa4
Deep Vasan <deepvasan10@gmail.com>
2022-12-27 21:53:54 +0530

    Added Teacher Views

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Classwork from "./Student/components/Classwork";
 import Quizzes from "./Student/components/Quizzes";
 import Login from "./components/auth/login";
 import Logout from "./components/auth/logout";
+import StudentRecord from "./Teacher/components/StudentRecord";
+import TeacherQuiz from "./Teacher/components/TeacherQuiz";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/calender" element={<Calender />} />
         <Route path="/teacher" element={<TeacherMain />} />       
         <Route path="/quizzes" element={<TeacherQuizes />} />
+        <Route path="/studentrecord" element={<StudentRecord />} />
+        <Route path="/teacherquiz" element={<TeacherQuiz />} />
         <Route path="/quiz" element={<Quizzes />} />
         <Route path="/self" element={<Self />} />
         <Route path="/peer" element={<Peer />} />
