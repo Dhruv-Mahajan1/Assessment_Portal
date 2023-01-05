7d61fff0a601d09a9e9b37e85ff014fe2ef48e40
Haardik <ravat.1@iitj.ac.in>
2023-01-05 15:56:54 +0530

    Added Correect Answer model

--- Assessment_Portal/quizes/models.py
+++ Assessment_Portal/quizes/models.py
@@ -6,7 +6,7 @@ class Quiz(models.Model):
     quizId=models.AutoField(primary_key=True)
     name=models.CharField(max_length=200)
     dateCreated=models.DateField(auto_now_add=True)
-    mapped=models.BooleanField(default=False)
+    # mapped=models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -21,4 +21,13 @@ class Question(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CorrectAnswer(models.Model):
+    quizId= models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    questionId=models.ForeignKey(Question, primary_key=True, on_delete=models.CASCADE)
+    correctAnswer=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.correctAnswer
    
